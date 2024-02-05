082676bda8b133d4bcf3ae98124497fdb8953571
chaudhuree <chaudhuree00@gmail.com>
2024-02-05 19:22:26 +0600

    3️⃣ form submission to get input data in the handler for further usage

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -2,12 +2,23 @@ import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
 function LoginForm() {
-  const { register,control } = useForm();
+  const { register,control,handleSubmit } = useForm<FormValues>();
+  // typescript types
+  type FormValues = {
+    username: string;
+    email: string;
+    channel: string;
+  };
+
+  const onSubmit = (data:FormValues) => {
+    console.log(data);
+  };
+  
   return (
     <div>
     <h1>Login Form</h1>
 
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <label htmlFor="username">Username</label>
       <input type="text" id="username"  {...register("username")}/>
 
